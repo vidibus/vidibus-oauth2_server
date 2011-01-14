f1f66cd7c5cf5365b835f85b57aa1fbb1f1dc859
Andre Pankratz <andre@webwarelab.com>
2011-01-14 17:04:23 +0100

    Improved error descriptions

--- lib/vidibus/oauth2_server/extensions/controller.rb
+++ lib/vidibus/oauth2_server/extensions/controller.rb
@@ -7,12 +7,12 @@ module Vidibus
 
         # Authenticates user.
         def authenticate_user!
-          raise "Add a method authenticate_user! to your ApplicationController that authenticates the user that calls the OAuth actions."
+          raise "Add a method #authenticate_user! to your ApplicationController that authenticates the user."
         end
 
         # Returns Oauth2 client application matching given id.
         def oauth2_client(id)
-          raise "Add a method oauth2_client to your ApplicationController that returns the OAuth client application for given id."
+          raise "Add a method #oauth2_client to your ApplicationController that returns the OAuth client application for given id."
         end
       end
     end
