049c2ffd2b0871a28f7e0b4b1e3f8625751a4564
punkrats <andre@webwarelab.com>
2013-09-30 23:19:12 +0200

    Refactor users controller

--- app/controllers/oauth2/users_controller.rb
+++ app/controllers/oauth2/users_controller.rb
@@ -1,16 +1,22 @@
 class Oauth2::UsersController < Oauth2Controller
   before_filter :ensure_token!
-  before_filter :find_user
+  before_filter :ensure_user
 
   def show
-    attributes = @user.try!(:oauth_attributes) || @user.attributes.only(*%w[name email uuid])
+    attributes = user.try!(:oauth_attributes) || user.attributes.only(*%w[name email uuid])
     render :json => attributes
   end
 
   protected
 
-  def find_user
-    @user = User.where(:uuid => @access_token.user_id).first or render(:nothing => true, :status => :bad_request)
+  def user
+    @user ||= User.where(:uuid => @access_token.user_id).first
+  end
+
+  def ensure_user
+    unless user
+      render(:nothing => true, :status => :bad_request)
+    end
   end
 
   def ensure_token!
