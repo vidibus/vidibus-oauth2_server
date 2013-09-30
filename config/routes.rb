513636a511307a650b5ff52c5434f86463314574
punkrats <andre@webwarelab.com>
2013-09-30 23:17:41 +0200

    Respond to /oauth/token route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/oauth/authorize' => 'oauth2/authentication#authorize'
+  match '/oauth/token' => 'oauth2/authentication#access_token', :via => [:get, :post]
   match '/oauth/access_token' => 'oauth2/authentication#access_token', :via => [:get, :post]
   match '/oauth/oauth_token' => 'oauth2/authentication#access_token', :via => [:get, :post]
   get '/oauth/user' => 'oauth2/users#show'
